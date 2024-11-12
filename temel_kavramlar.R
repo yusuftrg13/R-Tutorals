66aa6c4893f40e32a10f36ef228afe8555e0cd4a
Yusuf Turgay <132002974+yusuftrg13@users.noreply.github.com>
2024-11-12 09:06:19 +0300

    Add files via upload

---
+++ temel_kavramlar.R
@@ -0,0 +1,50 @@
+#R'da degisken tanımlamak için <- operatörü kullanırız.= operatörüde geçerlidir
+
+#Sayısal bir degisken
+x=5
+y=10
+
+#Metin(karakter) degiskeni
+ad="Yusuf"
+soyad="Turgay"
+
+#Mantıksal bir degisken
+aktif<- TRUE
+devam = FALSE
+
+#R programlamada veri tiple
+a=4 #numeric
+b="R dili" #character
+c=TRUE
+d= 7L #Integer
+
+
+#Temel Operasyonlar
+
+t=10
+u=20
+
+toplam=t+u
+fark=t-u
+carpim=t*u
+bolum=t/u
+us_al=t^u
+mod=t%u
+
+
+# Karşılaştırma
+a == b     # Eşit mi? (FALSE)
+a != b     # Eşit değil mi? (TRUE)
+a > b      # Büyük mü? (TRUE)
+a < b      # Küçük mü? (FALSE)
+a >= b     # Büyük veya eşit mi? (TRUE)
+a <= b     # Küçük veya eşit mi? (FALSE)
+
+# Mantıksal işlemler
+x <- TRUE
+y <- FALSE
+
+x & y      # Ve (FALSE)
+x | y      # Veya (TRUE)
+!x         # Değil (FALSE)
+
